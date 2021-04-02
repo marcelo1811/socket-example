40e7f50bb3ca6ef400dc62af98e998727e48e94f
marcelo1811 <miyachimarcelo@gmail.com>
2021-04-02 14:43:03 -0300

    feat(client): add socket integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import { io } from "socket.io-client";
+
+const initialMessages = [
+  {
+    id: 1,
+    content: 'Mensagem 1',
+    username: 'teste',
+  },
+  {
+    id: 2,
+    content: 'Mensagem 2',
+    username: 'teste',
+  },
+]
 
 function App() {
+  const [messages, setMessages] = useState(initialMessages)
+  const [username, setUsername] = useState('')
+  const [message, setMessage] = useState('')
+  const socketUrl = 'http://localhost:3000'
+  const socket = useRef(null)
+  
+  useEffect(() => {
+    if (!socket.current) {
+      socket.current = io(socketUrl, {
+        reconnectionDelayMax: 10000,
+      })
+    }
+
+    socket.current.on('connect', () => {
+      console.log('Socket connected!')
+    })
+    socket.current.on('chat', (data) => {
+      console.log('teste')
+      setMessages(prevState => {
+        return [
+        ...prevState,
+        data
+      ]})
+    })
+
+    return () => {
+      socket.current.close()
+    }
+  }, [])
+
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    let newMessage = {
+      id: new Date().getTime(),
+      content: message,
+      username: username,
+    }
+
+    socket.current.emit('chat', newMessage)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>
+        <h3>Username</h3>
+        <input
+          type="text"
+          name='username'
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <h1>Mensagens</h1>
+      <div>
+        {messages.map(msg => {
+          return (
+            <div key={msg.id}>
+              <strong>{msg.username}</strong> - {msg.content}
+            </div>
+          )
+        })}
+        <form
+          onSubmit={handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <input
+            type="text"
+            name='message'
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button
+            type='submit'
+          >Enviar</button>
+        </form>
+      </div>
+    </>
   );
 }
 
